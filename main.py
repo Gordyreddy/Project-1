3224d851da24a902bfdd31e3e6725aefe1c9ba95
ArvindGotike <arvindreddy9711@gmail.com>
2024-07-17 16:36:20 +0530

    added score and high score plus scores user names as file names

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
-
-name=input("What is your name?\n")
+import os
+def file_exists(file):
+    return os.path.isfile(file)
+un=input("What is your name?\n")
+name=un.lower()
 
 print("")# prints a blank space 
 
-print(f"Hello, {name} let`s play rock paper scissor\n")
-starting="y"
+file = (f"user_data/{name}_score.txt")
+exists = file_exists(file)
+
+
+if(exists == True ):
+    def d():
+        with open (f"user_data/{name}_score.txt") as f:
+            highscorei=f.read()
+            if(highscorei!=""):
+                highscore=int(highscorei)
+            else:
+                highscore = 0
+        return highscore
+    highscore=d()
+    print(f"Hello {name} \nWelcome back :)\n")
+    print(f"Your previous high score is: {highscore}")
+    
+    print("")# prints a blank space 
+
+else:
+    def d():
+        highscore = 0
+        return highscore
+    print(f"Hey {name}\n")
+    print("welcome to rock paper scissor a fun and exciting game.\n")
+    print("Author: Arvind Gotike\n")
+highscore=d()
+score = 0
+starting = "y"
 while( starting=="y" or c.lower()=="y"):
-    u=input("Choose one ( Rock, Paper or scissor) \n")
+    u=input("Choose one \n( Rock, Paper or scissor) \n")
 
     print("")# prints a blank space 
 
@@ -29,31 +59,48 @@ while( starting=="y" or c.lower()=="y"):
                 print("Its a draw!")
             else:
                 if(a==1 and b==3):
-                    return "Computer won!"
+                    return "You lost!"
                 elif(a==1 and b==2):
                     return "You won!"
                 elif(a==2 and b==1):
-                    return "Computer won!"
+                    return "You lost!"
                 elif(a==2 and b== 3):
                     return "You won!"
                 elif(a==3 and b==2):
-                    return "Computer won!"
+                    return "You lost!"
                 elif(a==3 and b==1):
-                    return "Computer won!"
+                    return "You lost!"
+        o=output(a, b)
+        print(o)
 
-        print(output(a , b))
-        
         print("")# prints a blank space 
-        
+
+        if (o == "You won!" ):
+            score+=1
+            print(f"Your score is: {score}\n")
+            print(f"Your previous high score is: {highscore}")
+        elif(o == "You lost!"):
+            score=0
+            print(f"Your Score is: {score}\n")
+            print(f"Your previous high score is: {highscore}")
+        else:
+            print(f"Your Score is: {score}\n")
+            print(f"Your previous high score is: {highscore}")
+        if(score>=highscore):
+            with open(f"user_data/{name}_score.txt", "w") as wr:
+                wr.write(str(score))
+
+        print("")# prints a blank space 
+            
         c=input("Do you want to play again?( y/n )\n")
 
         print("")# prints a blank space 
-        
+            
         if(c=="y"):
             continue
-        
+            
         elif(c=="n"):
-            print(f"Thankyou for playing with me {name}")
+            print(f"Thankyou for playing {name}")
             break
 
         elif( c != "y" or c != "n" ):
@@ -61,18 +108,18 @@ while( starting=="y" or c.lower()=="y"):
             print("choose ( y ) for ( yes ) and choose ( n ) for ( no )")
 
             print("")# prints a blank space 
-            
+                
             c=input("Do you want to play again?( y/n )\n")
-        
+            
         if(c=="y"):
             continue
-        
+            
         else:
-            print(f"Thankyou for playing with me {name}")
+            print(f"Thankyou for playing {name}")
     elif( user_input != "rock" or user_input != "paper" or user_input != "scissor" ):
         print("Something went wrong")
-        print(f"Thankyou for playing with me {name}")
+        print(f"Thankyou for playing {name}")
         break
 else:
     print("Something went wrong")
-    print(f"Thankyou for playing with me {name}")
+    print(f"Thankyou for playing {name}")
